6dccd2ebcba4d7756530f862461d4db5a2a932e3
zhereloff <zhereloff2014@yandex.ru>
2024-06-03 18:14:57 +0300

    ReceiveDataFromHost
    Added uart echo bytes packet

--- TechoLabPrj/Core/RingBuffer/ringBuffer.h
+++ TechoLabPrj/Core/RingBuffer/ringBuffer.h
@@ -8,6 +8,8 @@
 #ifndef RINGBUFFER_RINGBUFFER_H_
 #define RINGBUFFER_RINGBUFFER_H_
 
+#define RECEIVE_PWM_PACKET_SIZE 9
+
 #include "main.h"
 
 class RingBuffer {
@@ -19,6 +21,24 @@ public:
     	volatile uint16_t read_index;
 	public:
     	RingBuffer(): write_index(0), read_index(0) {}
+
+    	uint8_t dma_rx_buffer[RECEIVE_PWM_PACKET_SIZE];
+
+    	uint16_t getWriteIndex() const {
+    	        return write_index;
+    	}
+
+    	uint16_t getReadIndex() const {
+    	        return read_index;
+    	}
+
+    	uint8_t* getWritePointer() {
+    	        return &buffer[write_index];
+    	}
+
+    	void advanceWriteIndex(uint16_t length) {
+    	        write_index = (write_index + length) % BUFFER_SIZE;
+    	}
 
     	bool write(uint8_t data);
     	bool read(uint8_t &data);
